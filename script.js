0da83baf825416b9ea6b905a41eb4f380f9ae0f8
Marcos-RN <marcosro1999@gmail.com>
2023-01-26 12:59:22 +0100

    Creating a sudoku game

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let count = 0;
-
-function increment() {
-  count++;
-  document.getElementById("count").innerHTML = count;
+var cells = document.getElementsByClassName("sudoku-cell");
+for (var i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("input", function(e) {
+    // Validate input here, for example, make sure the input is a number
+    // between 1 and 9 and that it doesn't conflict with any other numbers
+    // in the same row, column, or 3x3 block
+  });
 }
