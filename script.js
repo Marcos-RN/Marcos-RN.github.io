2f4f7b61c1812079e9abc2153d6e24bdd7c1545a
Marcos-RN <marcosro1999@gmail.com>
2023-01-26 13:11:07 +0100

    Adding functionality sudoku

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 var cells = document.getElementsByClassName("sudoku-cell");
+var solution = [[], [], [], [], [], [], [], [], []]; // 2D array to store the solution
+var userInput = [[], [], [], [], [], [], [], [], []]; // 2D array to store the user's input
+
 for (var i = 0; i < cells.length; i++) {
-  cells[i].addEventListener("input", function(e) {
-    // Validate input here, for example, make sure the input is a number
-    // between 1 and 9 and that it doesn't conflict with any other numbers
-    // in the same row, column, or 3x3 block
-  });
-}
+  var row = Math.floor(i / 9);
+  var col = i % 9;
+  userInput[row][col] = cells[i]; }
+
+  // cells[i].addEventListener("input", function(e) {
+    // Validate input here, for example, make sure the input is a number between 1 and 9
+    // and that it doesn't conflict
+
